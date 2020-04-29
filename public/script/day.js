15cf77d3cf8799a76d395cc69f49ad97356b8634
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-04-30 00:15:02 +0800

    use clone to get a new activity form rather than creating it from scratch in dom

--- public/script/day.js
+++ public/script/day.js
@@ -107,67 +107,73 @@ const addForm = () => {
 
     const dayBody = document.querySelector('.days-body');
 
-    // Create activity form
-    const activityFormDiv = document.createElement('div');
-    activityFormDiv.className = "activity-form-div"
-    activityFormDiv.classList.add(`form-div${formNum}`)
+    const activityForm = document.querySelector('.activity-form-div');
 
-    const activityForm = document.createElement('form');
-    activityForm.className = "activity-form"
+    let newActivityForm = activityForm.cloneNode(true);
 
-    const title = document.createElement('input');
-
-    title.setAttribute('type', 'text');
-    title.setAttribute('name', 'title');
-    title.setAttribute('placeholder', 'Activity');
-    title.className="activity-title"
-    title.classList.add(`title${formNum}`)
-
-    const venue = document.createElement('input');
-    venue.setAttribute('type', 'text');
-    venue.setAttribute('name', 'location');
-    venue.setAttribute('placeholder', 'location');
-    venue.className="activity-location"
-    venue.classList.add(`location${formNum}`)
-
-    const timeStart = document.createElement('input');
-    timeStart.setAttribute('type', 'time');
-    timeStart.setAttribute('name', 'time_start');
-    timeStart.className="activity-timestart"
-    timeStart.classList.add(`timestart${formNum}`)
-
-    const timeEnd = document.createElement('input');
-    timeEnd.setAttribute('type', 'time');
-    timeEnd.setAttribute('name', 'time_end');
-    timeEnd.className="activity-timeend"
-    timeEnd.classList.add(`timeend${formNum}`)
-
-    const notes = document.createElement('input');
-    notes.setAttribute('type', 'text');
-    notes.setAttribute('name', 'notes');
-    notes.setAttribute('placeholder', 'notes');
-    notes.className="activity-notes"
-    notes.classList.add(`notes${formNum}`)
-
-    const saveBtn = document.createElement('button');
-    saveBtn.setAttribute('type', 'button');
-    saveBtn.className = 'save-btn'
-    saveBtn.textContent = 'Save'
-    saveBtn.addEventListener('click', () => {
-        saveForm(formNum)
-    });
+    dayBody.insertBefore(newActivityForm, addActivityBtn);
 
 
-    activityForm.appendChild(title);
-    activityForm.appendChild(venue);
-    activityForm.appendChild(timeStart);
-    activityForm.appendChild(timeEnd);
-    activityForm.appendChild(notes);
-    activityForm.appendChild(saveBtn);
+    // // Create activity form
+    // const activityFormDiv = document.createElement('div');
+    // activityFormDiv.className = "activity-form-div"
+    // activityFormDiv.classList.add(`form-div${formNum}`)
 
-    activityFormDiv.appendChild(activityForm);
+    // const activityForm = document.createElement('form');
+    // activityForm.className = "activity-form"
 
-    dayBody.insertBefore(activityFormDiv, addActivityBtn);
+    // const title = document.createElement('input');
+
+    // title.setAttribute('type', 'text');
+    // title.setAttribute('name', 'title');
+    // title.setAttribute('placeholder', 'Activity');
+    // title.className="activity-title"
+    // title.classList.add(`title${formNum}`)
+
+    // const venue = document.createElement('input');
+    // venue.setAttribute('type', 'text');
+    // venue.setAttribute('name', 'location');
+    // venue.setAttribute('placeholder', 'location');
+    // venue.className="activity-location"
+    // venue.classList.add(`location${formNum}`)
+
+    // const timeStart = document.createElement('input');
+    // timeStart.setAttribute('type', 'time');
+    // timeStart.setAttribute('name', 'time_start');
+    // timeStart.className="activity-timestart"
+    // timeStart.classList.add(`timestart${formNum}`)
+
+    // const timeEnd = document.createElement('input');
+    // timeEnd.setAttribute('type', 'time');
+    // timeEnd.setAttribute('name', 'time_end');
+    // timeEnd.className="activity-timeend"
+    // timeEnd.classList.add(`timeend${formNum}`)
+
+    // const notes = document.createElement('input');
+    // notes.setAttribute('type', 'text');
+    // notes.setAttribute('name', 'notes');
+    // notes.setAttribute('placeholder', 'notes');
+    // notes.className="activity-notes"
+    // notes.classList.add(`notes${formNum}`)
+
+    // const saveBtn = document.createElement('button');
+    // saveBtn.setAttribute('type', 'button');
+    // saveBtn.className = 'save-btn'
+    // saveBtn.textContent = 'Save'
+    // saveBtn.addEventListener('click', () => {
+    //     saveForm(formNum)
+    // });
+
+
+    // activityForm.appendChild(title);
+    // activityForm.appendChild(venue);
+    // activityForm.appendChild(timeStart);
+    // activityForm.appendChild(timeEnd);
+    // activityForm.appendChild(notes);
+    // activityForm.appendChild(saveBtn);
+
+    // activityFormDiv.appendChild(activityForm);
+
 }
 
 addActivityBtn.addEventListener('click', addForm);
