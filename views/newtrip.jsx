55428c6dd0e451c53ef09d96dfcf0a42dff94a05
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-05-02 20:32:50 +0800

    able to use auto-complete on newtrip

--- views/newtrip.jsx
+++ views/newtrip.jsx
@@ -6,7 +6,7 @@ class Login extends React.Component {
 
     const newTripForm = (
             <form action='/addnewtrip' method='post' className='form'>
-                <input type="text" name="country" placeholder="country" className="country"></input><br></br>
+                <input type="text" name="country" placeholder="country" className="country" id="autocomplete" onFocus="geolocate()"></input><br></br>
                 <input type="text" name="name" placeholder="trip name" className="trip-name"></input><br></br>
                 <input type="date" name="date_start" className="date-start"></input>
                 <input type="date" name="date_end" className="date-end"></input><br></br>
@@ -20,6 +20,9 @@ class Login extends React.Component {
             <div className='new-trip'>
                 {newTripForm}
             </div>
+            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCUoCP1u86ZopN6JWDAWKi8LdOhWSke9v4&libraries=places&callback=initAutocomplete"
+        async defer></script>
+        <script src="/script/newtrip.js"></script>
         </DefaultLayout>
     );
   }
